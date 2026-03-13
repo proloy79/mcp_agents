8b702aee3d792133cdf9bc3f988aacc96a685962
Proloy Goswami <proloy79@gmail.com>
2026-03-13 21:45:33 +0000

    moved specs to config

--- src/incident_agent/tools/specs/__init__.py
+++
@@ -1,9 +0,0 @@
-from .retrieve_runbook import RETRIEVE_RUNBOOK_SPEC
-from .run_diagnostic import RUN_DIAGNOSTIC_SPEC
-from .summarize_incident import SUMMARIZE_INCIDENT_SPEC
-
-__all__ = [
-    "RUN_DIAGNOSTIC_SPEC",
-    "RETRIEVE_RUNBOOK_SPEC",
-    "SUMMARIZE_INCIDENT_SPEC"
-]
